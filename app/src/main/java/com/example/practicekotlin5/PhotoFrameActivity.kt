5837b2ef04c5c4ba63f4d8c5eb308fb9057fc9ad
KKW <yesrjsdn@gmail.com>
2022-03-19 20:59:30 +0900

    KotlinPractice_FastCampus_Chapter05

---
+++ app/src/main/java/com/example/practicekotlin5/PhotoFrameActivity.kt
@@ -0,0 +1,94 @@
+package com.example.practicekotlin5
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import java.util.*
+import kotlin.concurrent.timer
+
+class PhotoFrameActivity : AppCompatActivity() {
+
+    private val photoList = mutableListOf<Uri>()
+
+    private val photoImageView: ImageView by lazy {
+        findViewById(R.id.photoImageView)
+    }
+
+    private val backgroundPhotoImageView: ImageView by lazy {
+        findViewById(R.id.backgroundPhotoImageView)
+    }
+
+    private var currentPosition = 0
+
+    private var timer: Timer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_frame)
+
+        Log.d("PhotoFrame,", "onCreate()")
+
+        getPhotoUriFromIntent()
+    }
+
+    private fun getPhotoUriFromIntent() {
+
+        val size = intent.getIntExtra("photoListSize", 0)
+        for (i in 0..size) {
+            intent.getStringExtra("photo$i")?.let {
+                photoList.add(Uri.parse(it))
+            }
+        }
+    }
+
+    private fun startTimer() {
+        timer = timer(period = 5000) {
+            runOnUiThread {
+
+                Log.d("PhotoFrame,", "startTimer: 5초 경과")
+
+                val current = currentPosition
+                val next = if (photoList.size <= currentPosition + 1) 0 else currentPosition + 1
+
+                backgroundPhotoImageView.setImageURI(photoList[current])
+
+                photoImageView.alpha = 0f
+                photoImageView.setImageURI(photoList[next])
+                photoImageView.animate()
+                    .alpha(1.0f)
+                    .setDuration(1000)
+                    .start()
+
+                currentPosition = next
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("PhotoFrame,", "onStop: timer cancel()")
+
+        timer?.cancel()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("PhotoFrame,", "onStart: timer start()")
+
+        startTimer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("PhotoFrame,", "onDestroy: timer cancel()")
+
+        timer?.cancel()
+    }
+
+
+}
